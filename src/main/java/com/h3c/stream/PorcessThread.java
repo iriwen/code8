fc16390268a250d9c387947ed614c9c5db6ec254
iriwen <worker1301ux@gmail.com>
2019-08-05 17:36:20 +0800

    linux下线程号（spid）测试

---
+++ src/main/java/com/h3c/stream/PorcessThread.java
@@ -0,0 +1,29 @@
+package com.h3c.stream;
+
+public class PorcessThread {
+
+
+    public static void main(String[] args) throws InterruptedException {
+
+        System.out.println("main thread :" + Thread.currentThread().getName());
+        //Thread.sleep(10000);
+
+
+        new Thread(() ->
+        {
+            for (; ; ) {
+                System.out.println("thread 1 :" + Thread.currentThread().getName());
+            }
+
+        }).start();
+
+        new Thread(() ->
+        {
+            for (; ; ) {
+                System.out.println("thread 2 :" + Thread.currentThread().getName());
+            }
+
+        }).start();
+
+    }
+}

8846a6953ddc83e68c9c0194dac1178d8f9d06bf
iriwen <worker1301ux@gmail.com>
2019-11-01 17:17:44 +0800

    插件定制修改

--- src/main/java/com/h3c/entity/Apple.java
+++ src/main/java/com/h3c/entity/Apple.java
@@ -1,6 +1,9 @@
 package com.h3c.entity;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class Apple {
     private Integer id;
@@ -10,6 +13,13 @@ public class Apple {
 
     public Apple(Integer id, String name, BigDecimal money, Integer num) {
         this.id = id;
+        this.name = name;
+        this.money = money;
+        this.num = num;
+    }
+
+
+    public Apple(String name, BigDecimal money, Integer num) {
         this.name = name;
         this.money = money;
         this.num = num;
@@ -46,4 +56,23 @@ public class Apple {
     public void setNum(Integer num) {
         this.num = num;
     }
+
+
+    public static void main(String[] args) {
+        Apple a = new Apple("a", new BigDecimal(233), 22);
+        Apple b = new Apple(11, "b", new BigDecimal(1233), 23);
+        Apple c = new Apple(12, "c", new BigDecimal(123), 33);
+
+        List<Apple> apples = new ArrayList<>();
+        apples.add(a);
+        apples.add(b);
+        apples.add(c);
+        List<Apple> collect = apples.stream().filter(item -> item.getId() == null).collect(Collectors.toList());
+
+        collect.forEach(item -> item.setId(66));
+        for (Apple apple : apples) {
+            System.out.println(apple.getId());
+        }
+    }
+
 }
